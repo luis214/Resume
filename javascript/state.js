754345f0e37ee8a063dc8f0842e483638fe4265a
luiscarbaj <la.carbajal92@gmail.com>
2017-06-19 00:57:49 -0700

    changes

---
+++ javascript/state.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function(){
+
+  $('#about').click(function(){
+      document.getElementById("wrapper").style.display = "block";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "none";
+      $(".nav").find(".active").removeClass("active");
+      $("#about").addClass("active");
+  });
+  $('#carbon').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "block";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "none";
+      $(".nav").find(".active").removeClass("active");
+      $("#project").addClass("active");
+  });
+  $('#sus').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("si").style.display = "block";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "none";
+      $(".nav").find(".active").removeClass("active");
+      $("#project").addClass("active");
+  });
+  $('#experience').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("wrapper3").style.display = "block";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "none";
+      $(".nav").find(".active").removeClass("active");
+      $("#experience").addClass("active");
+  });
+  $('#contact').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("wrapper4").style.display = "block";
+      document.getElementById("cpp").style.display = "none";
+      $(".nav").find(".active").removeClass("active");
+      $("#contact").addClass("active");
+  });
+  $('#cc').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "block";
+      $(".nav").find(".active").removeClass("active");
+      $("#project").addClass("active");
+  });
+
+});
