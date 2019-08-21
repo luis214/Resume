d5191fcff8e3e32b9be87dce4071af6a2df39c19
Luis Carbajal <luis.carbajal@baesystems.com>
2019-08-21 17:48:54 -0500

    update

--- javascript/state.js
+++ javascript/state.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
       document.getElementById("cb").style.display = "none";
       document.getElementById("wrapper4").style.display = "none";
       document.getElementById("cpp").style.display = "none";
+	    document.getElementById("baer").style.display = "none";
+		  document.getElementById("res").style.display = "block";
       $(".nav").find(".active").removeClass("active");
       $("#about").addClass("active");
   });
@@ -18,6 +20,8 @@ $(document).ready(function(){
       document.getElementById("cb").style.display = "block";
       document.getElementById("wrapper4").style.display = "none";
       document.getElementById("cpp").style.display = "none";
+	    document.getElementById("baer").style.display = "none";
+		  document.getElementById("res").style.display = "block";
       $(".nav").find(".active").removeClass("active");
       $("#project").addClass("active");
   });
@@ -28,6 +32,8 @@ $(document).ready(function(){
       document.getElementById("si").style.display = "block";
       document.getElementById("wrapper4").style.display = "none";
       document.getElementById("cpp").style.display = "none";
+	    document.getElementById("baer").style.display = "none";
+		  document.getElementById("res").style.display = "block";
       $(".nav").find(".active").removeClass("active");
       $("#project").addClass("active");
   });
@@ -38,19 +44,35 @@ $(document).ready(function(){
       document.getElementById("wrapper3").style.display = "block";
       document.getElementById("wrapper4").style.display = "none";
       document.getElementById("cpp").style.display = "none";
+	    document.getElementById("baer").style.display = "none";
+		   document.getElementById("perr").style.display = "none";
+		     document.getElementById("res").style.display = "block";
       $(".nav").find(".active").removeClass("active");
-	   document.getElementById("res").style.display = "block";
       $("#experience").addClass("active");
   });
-  $('#contact').click(function(){
+  $('#bae').click(function(){
       document.getElementById("wrapper").style.display = "none";
       document.getElementById("si").style.display = "none";
       document.getElementById("cb").style.display = "none";
       document.getElementById("wrapper3").style.display = "none";
-      document.getElementById("wrapper4").style.display = "block";
+      document.getElementById("wrapper4").style.display = "none";
       document.getElementById("cpp").style.display = "none";
+	  document.getElementById("baer").style.display = "block";
+	     document.getElementById("perr").style.display = "none";
       $(".nav").find(".active").removeClass("active");
-      $("#contact").addClass("active");
+     // $("#project").addClass("active");
+  });
+   $('#per').click(function(){
+      document.getElementById("wrapper").style.display = "none";
+      document.getElementById("si").style.display = "none";
+      document.getElementById("cb").style.display = "none";
+      document.getElementById("wrapper3").style.display = "none";
+      document.getElementById("wrapper4").style.display = "none";
+      document.getElementById("cpp").style.display = "none";
+	  document.getElementById("baer").style.display = "none";
+	   document.getElementById("perr").style.display = "block";
+      $(".nav").find(".active").removeClass("active");
+     // $("#project").addClass("active");
   });
   $('#cc').click(function(){
       document.getElementById("wrapper").style.display = "none";
@@ -58,7 +80,9 @@ $(document).ready(function(){
       document.getElementById("cb").style.display = "none";
       document.getElementById("wrapper3").style.display = "none";
       document.getElementById("wrapper4").style.display = "none";
+	    document.getElementById("baer").style.display = "none";
       document.getElementById("cpp").style.display = "block";
+	   document.getElementById("perr").style.display = "none";
       $(".nav").find(".active").removeClass("active");
       $("#project").addClass("active");
   });
